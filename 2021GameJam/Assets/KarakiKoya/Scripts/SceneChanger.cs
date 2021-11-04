2c71df6aaa6ca2e58463beb714c2d26ae1907834
KoyaKaraki <action.gamer.k@gmail.com>
2021-11-04 14:15:21 +0900

    タイトルシーンをゆるーく作成 ついでにシーン遷移ボタンも作成、ただし遅延なし

---
+++ 2021GameJam/Assets/KarakiKoya/Scripts/SceneChanger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// シーン遷移機能
+/// </summary>
+public class SceneChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// シーン遷移する
+    /// </summary>
+    /// <param name="sceneName">遷移先のシーン名</param>
+    public void SceneChange(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
