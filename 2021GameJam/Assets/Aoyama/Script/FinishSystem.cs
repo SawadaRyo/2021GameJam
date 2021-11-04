497986b3c85c4efc6cde0557566b2869c4a9bd8d
aoyama_saito <saitomarimo1229@gmail.com>
2021-11-04 12:35:32 +0900

    finishManagerを追加

--- 2021GameJam/Assets/Aoyama/Script/FinishSystem.cs
+++ 2021GameJam/Assets/Aoyama/Script/FinishSystem.cs
@@ -1,18 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class FinishSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] UnityEvent finishEvent;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Block")
+        {
+            finishEvent.Invoke();
+        }
     }
 }
