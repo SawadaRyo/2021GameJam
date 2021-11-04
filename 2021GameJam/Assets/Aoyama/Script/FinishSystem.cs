9129b829db37d4ff25d4d453135ea5f244f5a1ec
aoyama_saito <saitomarimo1229@gmail.com>
2021-11-04 13:14:22 +0900

    MainSceneを変更

--- 2021GameJam/Assets/Aoyama/Script/FinishSystem.cs
+++ 2021GameJam/Assets/Aoyama/Script/FinishSystem.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
+[RequireComponent(typeof(BoxCollider2D))]
 public class FinishSystem : MonoBehaviour
 {
     [SerializeField] UnityEvent finishEvent;
