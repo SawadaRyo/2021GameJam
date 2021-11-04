cebc568dd86fd1eb28d9d956d94304fb7ef3847d
SR21C <rysawad@gmail.com>
2021-11-04 13:25:14 +0900

    追加

--- 2021GameJam/Assets/Sawada/Script/Generator.cs
+++ 2021GameJam/Assets/Sawada/Script/Generator.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Generator : MonoBehaviour
 {
     [SerializeField] GameObject[] blocks;
     [SerializeField] Transform genaratePoint;
