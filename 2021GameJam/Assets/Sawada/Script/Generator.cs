9850fafec1334d6d3e58e3df0dab0625208043c1
SR21C <rysawad@gmail.com>
2021-11-04 14:15:10 +0900

    ジェネレートシステムを調整しました。

--- 2021GameJam/Assets/Sawada/Script/Generator.cs
+++ 2021GameJam/Assets/Sawada/Script/Generator.cs
@@ -1,15 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Generator : MonoBehaviour
 {
     [SerializeField] GameObject[] blocks;
     [SerializeField] Transform genaratePoint;
+    [SerializeField] ScoreSystem scoreSystem;
+    [SerializeField] float distance;
     int beforeBlock;
-    
+    GameObject[] block;
+
+    private void Awake()
+    {
+        scoreSystem = GetComponent<ScoreSystem>();
+        block = GameObject.FindGameObjectsWithTag("Block");
+    }
+    private void FixedUpdate()
+    {
+        var nearblock = block.OrderBy(x => Vector3.Distance(new Vector3(0,x.transform.position.y,0), new Vector3(0,this.transform.position.y,0))).LastOrDefault();
+        Transform bp = nearblock.GetComponent<Transform>();
+        Vector3 gbp = this.gameObject.transform.position;
+        gbp.y = bp.position.y + distance;
+    }
     void Generate()
     {
+        scoreSystem.AddScore();
         var gb = Random.Range(0, blocks.Length-1);
         int judge = Judge(gb);
         Instantiate(blocks[judge], genaratePoint.position,Quaternion.identity);
